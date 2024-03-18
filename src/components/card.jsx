836ceac00b9b44384cea25fbd6c0c460c47ffe2a
nitrogen555 <aomloveoom333@gmail.com>
2024-03-18 18:01:27 +0700

    first commit

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,16 +2,16 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const BakeryComponent = () => {
-  const [bakeryData, setBakeryData] = useState([]);
+  const [bekeryData, setBekeryData] = useState([]);
 
   useEffect(() => {
-    fetchBakeryData();
+    fetchData();
   }, []);
 
-  const fetchBakeryData = async () => {
+  const fetchData = async () => {
     try {
-      const response = await axios.get('http://localhost:8000/bekery/showbekery'); 
-      setBakeryData(response.data);
+      const response = await axios.get('http://localhost:8000/bekery/showbekery');
+      setBekeryData(response.data);
     } catch (error) {
       console.error('เกิดข้อผิดพลาดในการโหลดข้อมูล bakery:', error);
     }
@@ -19,13 +19,13 @@ const BakeryComponent = () => {
 
   return (
     <div>
-      <h1>รายการ Bakery</h1>
+      <h1>Bakery</h1>
       <div>
-        {bakeryData.map(bakery => (
-          <div key={bakery.id}>
-            <img src={bakery.image_bakery} alt={bakery.bekeryname} />
-            <h2>{bakery.bekeryname}</h2>
-            <p>{bakery.description}</p>
+        {bekeryData.map((bekery) => (
+          <div key={bekery.bekery_id}>
+            <img src={`http://localhost:8000/uploads/${bekery.image_bekery}`} alt={bekery.bekeryname} />
+            <h2>{bekery.bekeryname}</h2>
+            <p>{bekery.description}</p>
           </div>
         ))}
       </div>
--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -2,19 +2,13 @@ import React from 'react'
 import Bekery from '../components/card'
 
 
-
-
 export default function Home() {
-
-
   return (
     <>
       <div style={{ backgroundColor: 'rgb(36,92,116)', minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <div>
-
-        </div>
         <Bekery />
-
+        </div>
       </div>
     </>
   )
